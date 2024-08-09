cc5806bed8af24161463caa6ed9e7aa54ba3a3a8
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-08-09 16:16:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    const selectedOptionIndex = colorSelect.selectedIndex;
+
+    if (selectedOptionIndex !== -1) { // Check if an option is selected
+        colorSelect.remove(selectedOptionIndex);
+    }
+}
